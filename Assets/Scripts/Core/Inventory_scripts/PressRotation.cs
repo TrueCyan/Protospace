b76ed63d900d7acef151b4e3f0d2b2b4bbef6834
genius5375 <52522719+genius5375@users.noreply.github.com>
2020-12-09 08:17:13 +0900

    키위에 프레스 버튼 뜨게 구현

---
+++ Assets/Scripts/Core/Inventory_scripts/PressRotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressRotation : MonoBehaviour
+{   Transform cam;
+    public GameObject key;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam=Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(transform.position+cam.rotation*Vector3.forward,cam.rotation*Vector3.up);
+        if(key.activeSelf==false){
+            gameObject.SetActive(false);
+        }
+    }
+}
