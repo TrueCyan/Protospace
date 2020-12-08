6e4031b79cb4b5eb83c578ba17566ef8c13d2d1a
TrueCyan <sejin1011@kaist.ac.kr>
2020-12-09 05:55:10 +0900

    열쇠, 문, 인벤토리 새로 만듦

---
+++ Assets/Scripts/InventoryDisplayer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryDisplayer : MonoBehaviour
+{
+    public GameObject SlotPrefab;
+
+    private Image _slotImage;
+
+    private List<Image> _slots = new List<Image>();
+
+    public void Create(int slotNum)
+    {
+        _slotImage = SlotPrefab.GetComponent<Image>();
+        var width = _slotImage.rectTransform.rect.width;
+
+        float startPos = -width * (slotNum - 1) / 2;
+
+        for(var i = 0; i < slotNum; i++)
+        {
+            var obj = Instantiate(SlotPrefab, transform);
+            var rect = obj.GetComponent<RectTransform>();
+            rect.anchoredPosition = new Vector2(startPos + i * width, rect.anchoredPosition.y);
+            _slots.Add(obj.transform.GetChild(0).GetComponent<Image>());
+        }
+    }
+
+    public void ImageUpdate(List<ItemInfo> info)
+    {
+        Clear();
+        for (var i = 0; i < Mathf.Min(_slots.Count, info.Count); i++)
+        {
+            _slots[i].sprite = info[i].ItemImage;
+            _slots[i].color = info[i].ItemColor;
+        }
+    }
+
+    public void Clear()
+    {
+        foreach (var slot in _slots)
+        {
+            slot.sprite = null;
+            slot.color = Color.clear;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Item : MonoBehaviour
+{
+    public enum Type
+    {
+        key,
+        other
+    }
+    public Sprite ItemImage;
+    public Type ItemType;
+    public Color ItemColor;
+    public GameObject ItemObj;
+    public Material DefaultMat;
+    public float FloatPeriod = 1;
+    public float FloatAmp = 0.2f;
+    public float RotateSpeed = 30f;
+    private float _time;
+    private Renderer _renderer;
+    private Material _keyMat;
+
+    private void Start()
+    {
+        _renderer = ItemObj.GetComponent<Renderer>();
+        _keyMat = new Material(DefaultMat);
+        _renderer.material = _keyMat;
+    }
+
+    private void Update()
+    {
+        if (_keyMat.color != ItemColor)
+        {
+            _keyMat.color = ItemColor;
+        }
+
+        if (Application.isPlaying)
+        {
+            _time += Time.deltaTime;
+            if (_time > FloatPeriod) _time -= FloatPeriod;
+            ItemObj.transform.localPosition = new Vector3(0, FloatAmp * Mathf.Sin(_time / FloatPeriod * Mathf.PI * 2), 0);
+            ItemObj.transform.Rotate(Vector3.up, RotateSpeed * Time.deltaTime);
+        }
+    }
+
+    public ItemInfo Get()
+    {
+        var info = new ItemInfo(ItemImage, ItemType, ItemColor);
+        gameObject.SetActive(false);
+        return info;
+    }
+}
+
+public class ItemInfo
+{
+    public Sprite ItemImage;
+    public Item.Type ItemType;
+    public Color ItemColor;
+
+    public ItemInfo(Sprite img, Item.Type type, Color col)
+    {
+        ItemImage = img;
+        ItemType = type;
+        ItemColor = col;
+    }
+}
