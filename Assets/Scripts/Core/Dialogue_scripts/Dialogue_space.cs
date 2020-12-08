8600163dc315418f866afef787c72ef96751d1d9
genius5375 <52522719+genius5375@users.noreply.github.com>
2020-12-09 02:29:33 +0900

    Dialogue
    대화창

---
+++ Assets/Scripts/Core/Dialogue_scripts/Dialogue_space.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue_space : MonoBehaviour
+{
+    public GameObject talk;
+    void OnTriggerEnter(Collider col){
+        if(col.gameObject.tag=="Player"){
+            var sys =talk.gameObject.GetComponent<Dialogue_activate>();
+            sys.Activate=true;
+           gameObject.SetActive(false);
+        }
+    }
+}
